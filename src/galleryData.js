e9fe17a11dbff02f1a6afae78301917f464d0fc0
avarenee <abiery@wesleyan.edu>
2022-09-18 16:06:11 -0400

    Fix gallery data for local dev, align right nav properly

--- src/galleryData.js
+++ src/galleryData.js
@@ -18,7 +18,7 @@ import vampire from './gallery/vampire.jpg';
 const gallery = {
     galleryItems: [
         {
-            image: dullahan.src,
+            image: dullahan,
             title: "Dullahan",
             model: "Dahlia",
             year: 2019,
@@ -28,7 +28,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: gestalt.src,
+            image: gestalt,
             title: "Gestalt/Frankenstein's Monster",
             model: "Ashanti",
             year: 2019,
@@ -38,7 +38,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: jerseydevil.src,
+            image: jerseydevil,
             title: "The Jersey Devil",
             model: "Virgil",
             year: 2022,
@@ -48,7 +48,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: jinni.src,
+            image: jinni,
             title: "Jinni",
             model: "Nik",
             year: 2022,
@@ -58,7 +58,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: kit.src,
+            image: kit,
             title: "Tamamo-no-mae",
             model: "Kitsune",
             year: 2020,
@@ -68,7 +68,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: manananggal.src,
+            image: manananggal,
             title: "Manananggal",
             model: "Amanda",
             year: 2021,
@@ -79,7 +79,7 @@ const gallery = {
             "additionalInfo": "diptych"
         },
         {
-            image: manticore.src,
+            image: manticore,
             title: "Manticore",
             model: "Blaise",
             year: 2019,
@@ -89,7 +89,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: medusa.src,
+            image: medusa,
             title: "Medusa",
             model: "Louisa Ashe",
             year: 2021,
@@ -100,7 +100,7 @@ const gallery = {
             units: "ft"
         },
         {
-            image: minotaur.src,
+            image: minotaur,
             title: "Minotaur",
             model: "Reginald",
             year: 2020,
@@ -111,7 +111,7 @@ const gallery = {
             units: "ft"
         },
         {
-            image: reptilian.src,
+            image: reptilian,
             title: "Reptilian",
             model: "self portrait",
             year: 2020,
@@ -121,7 +121,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: selkie.src,
+            image: selkie,
             title: "Selkie",
             model: "Rain",
             year: 2021,
@@ -131,7 +131,7 @@ const gallery = {
             units: "cm"
         },
         {
-            image: siren.src,
+            image: siren,
             title: "Siren",
             model: "Juno Tempest",
             year: 2019,
@@ -141,7 +141,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: sphinx.src,
+            image: sphinx,
             title: "Sphinx",
             model: "Leeat",
             year: 2022,
@@ -151,7 +151,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: succubus.src,
+            image: succubus,
             title: "Succubus",
             model: "🍑",
             year: 2021,
@@ -161,7 +161,7 @@ const gallery = {
             units: "in"
         },
         {
-            image: sylph.src,
+            image: sylph,
             title: "Sylph",
             model: "Aster",
             year: 2021,
@@ -171,7 +171,7 @@ const gallery = {
             units: "cm"
         },
         {
-            image: vampire.src,
+            image: vampire,
             title: "Valerie the Vampire",
             model: "Marina",
             year: 2016,
