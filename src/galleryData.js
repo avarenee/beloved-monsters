afbb40030d2dc9b1a60a1f029e71b64859d5c544
Janet Bruesselbach <janet.bruesselbach@gmail.com>
2023-03-29 19:00:22 -0400

    added centaur and newer images

--- src/galleryData.js
+++ src/galleryData.js
@@ -1,33 +1,58 @@
 const publicUrl = process.env.PUBLIC_URL === "" ? "" : "https://bruesselbach.com/belovedmonsters";
 
-const dullahan = "55cc0dae05cec404e5caea7d0ee9e778.jpg";
-const gestalt = "af4d4c585e2e7f03b27886a68f76969f.jpg";
-const jerseydevil = "40ac0bf4b757aa347f3b4d17e2ec5c74.jpg";
-const jinni = "dca48320574bc8276440ca944776aaf9.jpg";
-const kit = "d990a5396b14205d53700a3a2e3b2d18.jpg";
-const manananggal = "b5c0081ef74b7e94fd8ebb5fb84ea5c1.jpg";
-const manticore = "1ad2965514c77a98a54a67b03afe201f.png";
-const medusa = "ed487064ebf61db2a5bfbdd6601acae5.jpg";
-const minotaur = "0298d6c7a2da8af9dc280c92f6a65855.jpg";
-const reptilian = "3aa0caa7b35c08c91b6f2e30423fea1a.jpg";
-const selkie = "faefc5423424d9a49446485b4edfc1e3.jpg";
-const siren = "867739ab95bd2c46aadd80ab5a8f497e.jpg";
-const sphinx = "a78c2b90ce9dbe7a1d5c0717ddca683e.jpg";
-const succubus = "bed33ba654fb582653c358440f5bf425.jpg";
-const sylph = "356d8da7cfe56b5cf81763012a8ee63c.jpg";
-const vampire = "85d4322ceedca3fed5a04ff30129754e.jpg";
+const dullahan = "dullahan.jpg";
+const gestalt = "gestalt.jpg";
+const jerseydevil = "jerseydevil.jpg";
+const jinni = "jinni.jpg";
+const kit = "kit.jpg";
+const manananggal = "manananggal.jpg";
+const manticore = "manticore.png";
+const medusa = "medusa.jpg";
+const minotaur = "minotaur.jpg";
+const reptilian = "reptilian.jpg";
+const selkie = "selkie.jpg";
+const siren = "siren.jpg";
+const sphinx = "sphinx.jpg";
+const succubus = "succubus.jpg";
+const sylph = "sylph.jpg";
+const vampire = "vampire.jpg";
 const fairy = "fairy.jpg";
+const zombie = "zombie.jpg";
+const furies = "furies.jpg";
+const werewolf = "werewolf.jpg";
+const angel = "angel.jpg";
+const centaur = "centaur.jpg;"
 
 
 const gallery = {
     galleryItems: [
-        {
+                {
+            image: publicUrl + "/" + vampire,
+            title: "Valerie the Vampire",
+            model: "Marina",
+            year: 2016,
+            medium: "oil on canvas",
+            height: 48,
+            width: 24,
+            units: "in"
+        },
+		{
             image: publicUrl + "/" + dullahan,
             title: "Dullahan",
             model: "Dahlia",
             year: 2019,
             medium: "oil on canvas",
             height: 36,
+            width: 24,
+            units: "in"
+        },
+		        {
+            image: publicUrl + "/" + siren,
+            title: "Siren",
+            model: "Juno Tempest",
+            year: 2019,
+            medium: "oil on canvas",
+            height: 48,
             width: 24,
             units: "in"
         },
@@ -41,48 +66,7 @@ const gallery = {
             width: 32,
             units: "in"
         },
-        {
-            image: publicUrl + "/" + jerseydevil,
-            title: "The Jersey Devil",
-            model: "Virgil",
-            year: 2022,
-            medium: "oil on canvas",
-            height: 30,
-            width: 24,
-            units: "in"
-        },
-        {
-            image: publicUrl + "/" + jinni,
-            title: "Jinni",
-            model: "Nik",
-            year: 2022,
-            medium: "oil on canvas",
-            height: 36,
-            width: 24,
-            units: "in"
-        },
-        {
-            image: publicUrl + "/" + kit,
-            title: "Tamamo-no-mae",
-            model: "Kitsune",
-            year: 2020,
-            medium: "oil on canvas",
-            height: 36,
-            width: 36,
-            units: "in"
-        },
-        {
-            image: publicUrl + "/" + manananggal,
-            title: "Manananggal",
-            model: "Amanda",
-            year: 2021,
-            medium: "oil on canvas",
-            height: 40,
-            width: 24,
-            units: "in",
-            "additionalInfo": "diptych"
-        },
-        {
+		        {
             image: publicUrl + "/" + manticore,
             title: "Manticore",
             model: "Blaise",
@@ -92,18 +76,17 @@ const gallery = {
             width: 18,
             units: "in"
         },
-        {
-            image: publicUrl + "/" + medusa,
-            title: "Medusa",
-            model: "Louisa Ashe",
-            year: 2021,
-            "month": "May",
+		        {
+            image: publicUrl + "/" + kit,
+            title: "Tamamo-no-mae",
+            model: "Kitsune",
+            year: 2020,
             medium: "oil on canvas",
-            height: 3,
-            width: 2,
-            units: "ft"
+            height: 36,
+            width: 36,
+            units: "in"
         },
-        {
+		        {
             image: publicUrl + "/" + minotaur,
             title: "Minotaur",
             model: "Reginald",
@@ -114,7 +97,7 @@ const gallery = {
             width: 2,
             units: "ft"
         },
-        {
+		        {
             image: publicUrl + "/" + reptilian,
             title: "Reptilian",
             model: "self portrait",
@@ -123,38 +106,30 @@ const gallery = {
             height: 30,
             width: 21,
             units: "in"
+        },        
+		{
+            image: publicUrl + "/" + medusa,
+            title: "Medusa",
+            model: "Louisa Ashe",
+            year: 2021,
+            "month": "May",
+            medium: "oil on canvas",
+            height: 3,
+            width: 2,
+            units: "ft"
         },
-        {
-            image: publicUrl + "/" + selkie,
-            title: "Selkie",
-            model: "Rain",
+		       {
+            image: publicUrl + "/" + manananggal,
+            title: "Manananggal",
+            model: "Amanda",
             year: 2021,
             medium: "oil on canvas",
-            height: 80,
-            width: 60,
-            units: "cm"
-        },
-        {
-            image: publicUrl + "/" + siren,
-            title: "Siren",
-            model: "Juno Tempest",
-            year: 2019,
-            medium: "oil on canvas",
-            height: 48,
+            height: 40,
             width: 24,
-            units: "in"
+            units: "in",
+            "additionalInfo": "diptych"
         },
-        {
-            image: publicUrl + "/" + sphinx,
-            title: "Sphinx",
-            model: "Leeat",
-            year: 2022,
-            medium: "oil on canvas",
-            height: 32,
-            width: 36,
-            units: "in"
-        },
-        {
+		        {
             image: publicUrl + "/" + succubus,
             title: "Succubus",
             model: "🍑",
@@ -174,14 +149,44 @@ const gallery = {
             width: 60,
             units: "cm"
         },
-        {
-            image: publicUrl + "/" + vampire,
-            title: "Valerie the Vampire",
-            model: "Marina",
-            year: 2016,
+		        {
+            image: publicUrl + "/" + selkie,
+            title: "Selkie",
+            model: "Rain",
+            year: 2021,
             medium: "oil on canvas",
-            height: 48,
+            height: 80,
+            width: 60,
+            units: "cm"
+        },
+		        {
+            image: publicUrl + "/" + jinni,
+            title: "Jinni",
+            model: "Nik",
+            year: 2022,
+            medium: "oil on canvas",
+            height: 36,
             width: 24,
+            units: "in"
+        },
+        {
+            image: publicUrl + "/" + jerseydevil,
+            title: "The Jersey Devil",
+            model: "Virgil",
+            year: 2022,
+            medium: "oil on canvas",
+            height: 30,
+            width: 24,
+            units: "in"
+        },
+        {
+            image: publicUrl + "/" + sphinx,
+            title: "Sphinx",
+            model: "Leeat",
+            year: 2022,
+            medium: "oil on canvas",
+            height: 32,
+            width: 36,
             units: "in"
         },
 		        {
@@ -191,6 +196,55 @@ const gallery = {
             year: 2022,
             medium: "oil on canvas",
             height: 42,
+            width: 32,
+            units: "in"
+        },
+		{
+            image: publicUrl + "/" + zombie,
+            title: "Zombie",
+            model: "Artemis",
+            year: 2022,
+            medium: "oil on canvas",
+            height: 32,
+            width: 24,
+            units: "in"
+        },
+		{
+            image: publicUrl + "/" + furies,
+            title: "Furies",
+            model: "Christy, Rebekkah, Echo",
+            year: 2022,
+            medium: "oil on canvas",
+            height: 32,
+            width: 48,
+            units: "in"
+        },
+		{
+            image: publicUrl + "/" + werewolf,
+            title: "Werewolf",
+            model: "Zyaire",
+            year: 2023,
+            medium: "oil on canvas",
+            height: 30,
+            width: 24,
+            units: "in"
+        },
+		{
+            image: publicUrl + "/" + angel,
+            title: "Angel",
+            year: 2023,
+            medium: "oil on canvas",
+            height: 36,
+            width: 36,
+            units: "in"
+        },
+		{
+            image: publicUrl + "/" + centaur,
+            title: "Centaur",
+			model: "Brian",
+            year: 2023,
+            medium: "oil on canvas",
+            height: 46,
             width: 32,
             units: "in"
         },
